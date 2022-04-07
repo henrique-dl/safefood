71ce645f66b30a2de707e7ea0cd36352f122e9ea
Henrique <henrique.leite@unyleya.com.br>
2022-04-06 22:23:14 -0300

    Refactoring app

--- app/src/constants/index.js
+++
@@ -1,8 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import { COLORS, SIZES, FONTS } from "./theme";
-// import GOOGLE_API_KEY from "./maps"
-
-// export { icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY };
-export { icons, images, COLORS, SIZES, FONTS };
-
